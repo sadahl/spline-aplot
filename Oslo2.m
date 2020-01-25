507b7a6ce8d4ffaed1ce4b1b42a1ffcf3a0a3dd3
sadahl <salsdahl@gmail.com>
2020-01-25 10:54:36 +0100

    Added alternative knot insertion algorithm

---
+++ Oslo2.m
@@ -0,0 +1,31 @@
+function [bvec] = Oslo2(p, tauvec, cvec, tvec)
+%Oslo-Algorithm 2.
+%Knot conversion/insertion algorithm (alternative to Bohms algorithm):
+%Computes B-spline coefficients bvec of f in S_p_t.
+%cvec is the original coefficients of f in S_p_tau.
+%p is the degree of the spline. (p=3: cubic).
+
+m=length(tvec)-p-1;
+mu=1;
+bvec = zeros(1,m); %preallocation
+for i=1:m
+    mu=findMu(tauvec, tvec(i), mu);
+    if p==0
+        bvec(i)=cvec(mu);
+    else
+        cp=cvec(mu-p:mu);
+        offset=p-mu+1; %index offset for cp
+        for k = p:-1:1 %algorithm 2.20 modified
+            x=tvec(i+k);
+            for j = mu:-1:mu-k+1 % overwriting initial cp from the right
+                denom = tauvec(j+k)-tauvec(j);
+                cp(j+offset) = ( (tauvec(j+k)-x)*cp(j-1+offset) + ...
+                                (x-tauvec(j))*cp(j+offset) ) / denom;
+            end
+        end
+        bvec(i) = cp(p+1);
+    end % end if
+end % end for
+
+end %end function
+

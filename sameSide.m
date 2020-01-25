3b586292ccb07764fcd371b90c1fe5aad854c847
sadahl <salsdahl@gmail.com>
2020-01-25 10:37:22 +0100

    Changed variable names to avoid ambiguity

--- sameSide.m
+++ sameSide.m
@@ -1,21 +1,21 @@
-function [ bool ] = sameSide( cs,ce,pi,pj )
+function [ bool ] = sameSide( cs,ce,p1,p2 )
 %[ bool ] = sameSide( cs,ce,pi,pj )
 %   Checks whether points pi, pj are on the same side of line (cs,ce).
 %   Returns true if pi and pj are on the same side
 %   Returns false if not or if one of the points is exactly on  (cs,ce).
 
-dpi=pi-cs;
-dpj=pj-cs;
+dp1=p1-cs;
+dp2=p2-cs;
 dp=ce-cs;
 
 % cross requires dimension 3
-if length(pi)==2
-    dpi(:,3)=0;
-    dpj(:,3)=0;
+if length(p1)==2
+    dp1(:,3)=0;
+    dp2(:,3)=0;
     dp(:,3)=0;
 end
 
-bool = ( dot(cross(dpi,dp),cross(dpj,dp))>0 );
+bool = ( dot(cross(dp1,dp),cross(dp2,dp))>0 );
 
 end
 

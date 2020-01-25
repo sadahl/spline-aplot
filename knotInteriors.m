b93c447c203aa79c7914e31840901051268d81c0
sadahl <sadahl@math.uio.no>
2020-01-25 10:59:16 +0100

    Delete knotInteriors.m

--- knotInteriors.m
+++
@@ -1,9 +0,0 @@
-function [ xx ] = knotInteriors( sp )
-%Returns the knot interiors of the spline sp
-% Part of insertFunction family used by splinePlot2
-
-xx=setdiff(sp.knots,[sp.knots(1) sp.knots(sp.number+sp.order)])
-% xx= [xx xx xx]
-
-end
-
